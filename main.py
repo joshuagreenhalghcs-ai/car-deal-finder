c872fb078a85c177f86fb46efe96017d05204794
Joshua Greenhalgh <joshua.greenhalgh.cs@gmail.com>
2026-05-16 14:18:07 +0100

    Added total profit tracking

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ print("Car Deal Finder")
 
 running = True
 
+total_profit = 0
+cars_checked = 0
+
 while running:
 
     buy_price = int(input("Enter buy price: "))
     sell_price = int(input("Enter sell price: "))
 
     profit = sell_price - buy_price
+
+    total_profit = total_profit + profit
+    cars_checked = cars_checked + 1
 
     print("Profit:")
     print(profit)
@@ -27,9 +33,16 @@ while running:
     else:
         print("Bad deal")
 
+    print("Total profit so far:")
+    print(total_profit)
+
+    print("Cars checked:")
+    print(cars_checked)
+
     again = input("Check another car? yes/no: ")
 
     if again == "no":
         running = False
 
 print("Program ended")
+average_profit = total_profit / cars_checked
