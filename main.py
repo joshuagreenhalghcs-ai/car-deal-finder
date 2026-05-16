c3eb0efb420ae4eb4292fc39cd991e7144e07862
Joshua Greenhalgh <joshua.greenhalgh.cs@gmail.com>
2026-05-16 14:15:59 +0100

    Added loop for multiple car checks

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 print("Car Deal Finder")
 
-buy_price = int(input("Enter buy price: "))
-sell_price = int(input("Enter sell price: "))
+running = True
 
-profit = sell_price - buy_price
+while running:
 
-print("Profit:")
-print(profit)
+    buy_price = int(input("Enter buy price: "))
+    sell_price = int(input("Enter sell price: "))
 
-if profit >= 2000:
-    print("Amazing deal!")
+    profit = sell_price - buy_price
 
-elif profit >= 1000:
-    print("Good deal!")
+    print("Profit:")
+    print(profit)
 
-elif profit > 0:
-    print("Small profit")
+    if profit >= 5000:
+        print("Perfect deal!")
 
-else:
-    print("Bad deal")
-    
+    elif profit >= 2000:
+        print("Amazing deal!")
+
+    elif profit >= 1000:
+        print("Good deal!")
+
+    elif profit > 0:
+        print("Small profit")
+
+    else:
+        print("Bad deal")
+
+    again = input("Check another car? yes/no: ")
+
+    if again == "no":
+        running = False
+
+print("Program ended")
